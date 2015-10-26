e331392a00c72b538f3d59c2f7b207e553987daa
antogerva <antogerva@gmail.com>
2015-10-25 22:52:13 -0400

    Added support for complex commands.

--- emuHostUDP.lua
+++ emuHostUDP.lua
@@ -1,4 +1,4 @@
-package.cpath = ";./?51.dll;"..package.cpath
+package.cpath = ";./?51.dll;./debug/?.dll;"..package.cpath
 package.path = ";./socket/?.lua;"..package.path
 
 --You can download the iup library from:
@@ -13,40 +13,104 @@ local portHost = 51424;
 local portClient = 51425;
 
 server:setsockname("localhost", portHost) 
-server:settimeout(1);
+server:settimeout(0);
 print("Server is up and can accept connetion");
 
-local fps = 44;
+local remExecBuilder=require('remExec');
+
+local fps = 111;
 local timer = iup.timer{time=(1000/fps), run="YES"};
 function timer:action_cb()
   start();
+  --poll();
 end
 
+--Use the wxlua debugger to fix any single debbuging session
+--require("wx");
+--wxlua.LuaStackDialog();
+
+local queueCmd={};
 local isRcpt=false;
-local dataToSend="Data!";
+local remoteCmdToExecute=nil;
+local dataToSend="New command executed.";
+
+
+function poll()
+  --socket.poll(socket,'*r');
+  server:setpeername("localhost", portClient);
+  local line, err = client:receive();
+  local rcptValue = tostring(line);
+
+  --print("got: "..rcptValue)   
+  if(rcptValue=="send") then
+    server:send(dataToSend);
+    --print("done ".. rcptValue)
+    isRcpt=true;
+  elseif(rcptValue~=nil and rcptValue~="nil") then
+    server:send(dataToSend);
+    --print("added "..rcptValue)
+    table.insert(queueCmd, remExecBuilder(rcptValue));
+  end
+end
+
 function start()
-  local canread = socket.select({server}, {server}, 1);    
+  local canread = socket.select({server}, nil, 0);    
   for _,client in ipairs(canread) do
     server:setpeername("localhost", portClient);
     local line, err = client:receive();
-    local rcptValue =  tostring(line);
+    local rcptValue = tostring(line);
 
-    if(rcptValue ~=nil and rcptValue ~="nil") then
+    --print("got: "..rcptValue)   
+    if(rcptValue=="send") then
       server:send(dataToSend);
+      --print("done ".. rcptValue)
       isRcpt=true;
+    elseif(rcptValue~=nil and rcptValue~="nil") then
+      --server:send(dataToSend);
+      --print("added "..rcptValue)
+      table.insert(queueCmd, remExecBuilder(rcptValue));
     end
   end
 end
 
+--emu.frameadvance();
+--emu.yield();
+client.speedmode(100);
 client.pause();
+
+--gui.drawText(10,10,"Test");
+local idleCount=0;
 while true do
   if(isRcpt==true) then
-    dataToSend= "hola!! " .. gameinfo.getromname();
+    idleCount=0;
+    --TODO: Send back the actual data returned.
+    --example:
+    --dataToSend= "game name: " .. gameinfo.getromname();
+    dataToSend = "New Command command executed with success.";
     isRcpt=false;
-    client.unpause();
-    emu.frameadvance();
-    emu.frameadvance();
-    client.pause();
+    if(client.ispaused()) then
+      client.unpause();
+    end
+    for i, value in ipairs(queueCmd) do
+      local remCmd=queueCmd[i];
+      queueCmd[i]=nil;
+      if(remCmd.multiArgs=="ping") then
+        print("pong");
+        dataToSend="pong";
+      else
+        remCmd:parse();
+        --remCmd:print(); -- show all the params and function used.
+        remCmd:exec();
+      end
+    end
+    queueCmd={};
+  elseif(client.ispaused()~=true) then
+    idleCount = idleCount+1;
+    if(idleCount>100) then
+      --Put back to pause when there's no message received
+      --when passing over a certain threshold
+      client.pause();
+    end
   end
   emu.yield();
 end
